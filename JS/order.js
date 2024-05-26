b1dd9d48cc2ec46354e61565e400700b5da24d80
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-25 20:14:15 -0700

    Adds price of size onto receipt

--- JS/order.js
+++ JS/order.js
@@ -4,10 +4,25 @@ function getReceipt() {
     let runningTotal = 0;
     let sizeTotal = 0;
     let sizeArray = document.getElementsByClassName("size");
+    
     for (let i = 0; i < sizeArray.length; i++) {
-        id (sizeArray[i].checked) {
-            let selectedSize = sizeArray[i].value;
+        if (sizeArray[i].checked) {
+            var selectedSize = sizeArray[i].value;
             text = heading + "<br>" + selectedSize;
         }
     }
+
+    if (selectedSize === 'Personal Pizza') { sizeTotal = 10;}
+    else if (selectedSize === 'Small Pizza') {sizeTotal = 12;}
+    else if (selectedSize === 'Medium Pizza') {sizeTotal = 14;}
+    else if (selectedSize === 'Large Pizza') {sizeTotal = 16;}
+    else if (selectedSize === 'Extra Large Pizza') {sizeTotal = 18;}
+
+    runningTotal = sizeTotal;
+
+    console.log("Receipt so far = " + text);
+    console.log(selectedSize + " = $"+sizeTotal + ".00");
+    console.log("Subtotal: $" + runningTotal + ".00");
+    
+    getTopping(runningTotal, text);
 }
