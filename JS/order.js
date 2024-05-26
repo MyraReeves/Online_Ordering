eda0d1bcda0d0e8687f7ac0b41caf36f28fbd437
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-25 20:48:11 -0700

    Adds cost of toppings to total amount due

--- JS/order.js
+++ JS/order.js
@@ -1,3 +1,4 @@
+
 function getReceipt() {
     const heading = "<h4>Your Order:</h4>"
     let text;
@@ -8,15 +9,15 @@ function getReceipt() {
     for (let i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked) {
             var selectedSize = sizeArray[i].value;
-            text = heading + "<br>" + selectedSize;
+            text = "<br> <hr>" + heading + "<br>" + selectedSize + " with: <br>";
         }
     }
 
     if (selectedSize === 'Personal Pizza') { sizeTotal = 10;}
-    else if (selectedSize === 'Small Pizza') {sizeTotal = 12;}
-    else if (selectedSize === 'Medium Pizza') {sizeTotal = 14;}
-    else if (selectedSize === 'Large Pizza') {sizeTotal = 16;}
-    else if (selectedSize === 'Extra Large Pizza') {sizeTotal = 18;}
+    else if (selectedSize === 'Small pizza') {sizeTotal = 12;}
+    else if (selectedSize === 'Medium pizza') {sizeTotal = 14;}
+    else if (selectedSize === 'Large pizza') {sizeTotal = 16;}
+    else if (selectedSize === 'Extra Large pizza') {sizeTotal = 18;}
 
     runningTotal = sizeTotal;
 
@@ -24,5 +25,34 @@ function getReceipt() {
     console.log(selectedSize + " = $"+sizeTotal + ".00");
     console.log("Subtotal: $" + runningTotal + ".00");
     
-    getTopping(runningTotal, text);
+    getToppings(runningTotal, text);
 }
+
+
+
+function getToppings(runningTotal, text) {
+    let toppingTotal = 0;
+    let selectedTopping = [];
+    let toppingArray = document.getElementsByClassName('toppings');
+    for (let i = 0; i < toppingArray.length; i++) {
+        if (toppingArray[i].checked) {
+            selectedTopping.push(toppingArray[i].value);
+            console.log("Selected topping: " + toppingArray[i].value);
+            text = text + toppingArray[i].value + "<br>"
+        }
+    }
+    let toppingCount = selectedTopping.length;
+
+    // First topping is "free"
+    if (toppingCount > 1) { toppingTotal = (toppingCount - 1); }
+    else {toppingTotal = 0;}
+
+    runningTotal = (runningTotal + toppingTotal);
+    console.log("Total number of toppings = " + toppingCount);
+    console.log(toppingCount + ' additional toppings minus 1 "free" topping = ' + '$' + toppingTotal + '.00');
+    console.log("Toppings chosen:" + text);
+    console.log("Subtotal: " + "$" + runningTotal + ".00");
+
+    document.getElementById("showText").innerHTML = text;
+    document.getElementById("totalPrice").innerHTML = "<h4>Total due: <strong>$" + runningTotal + ".00" + "</strong></h4>";
+};
